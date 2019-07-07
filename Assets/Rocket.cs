9e5a7fdb4312961218b1cfc909b58b8afe8cd0b6
Tyler Pearson <me@marktpearson.com>
2019-07-07 16:58:24 -0700

    added audio

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,12 +7,13 @@ public class Rocket : MonoBehaviour
 {
 
     Rigidbody rigidBody;
+    AudioSource rocketSound;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-        
+        rocketSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,8 +27,18 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
+    
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!rocketSound.isPlaying)
+            {
+                rocketSound.Play();
+            }
+        } else
+        {
+            rocketSound.Stop();
         }
+
+       
 
         if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
